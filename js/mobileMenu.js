7a4c86d2210c7a560f2607ad608212add543df3b
onlinesatish <34268479+onlinesatish@users.noreply.github.com>
2019-05-25 13:08:22 +0530

    Create mobileMenu.js

---
+++ js/mobileMenu.js
@@ -0,0 +1,21 @@
+//if you want mobile menu goes below navbar then uncomment this code.
+				/*
+				function setMenuTop(){
+					var navOuterHeight = $(".navbar").outerHeight();
+					$(".mobileMenu").css("top", navOuterHeight)
+				}
+				setMenuTop();
+				$(window).resize(function (){
+					setMenuTop();
+				});
+				$(".navbar .navbar-toggler").on("click", function(){
+					setMenuTop();
+				});
+				*/
+				
+				$(".navbar-toggler, .overlay").on("click", function(){
+					$(".mobileMenu, .overlay").toggleClass("open");
+					$(".hamburger").toggleClass("is-active");
+				});
+			});
+			
